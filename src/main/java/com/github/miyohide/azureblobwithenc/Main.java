18ac255f9d90c5ad1b5aa9ba808992c47b61273b
Hidenori Miyoshi <miyohide@gmail.com>
2022-03-20 20:07:18 +0900

    create blob

--- src/main/java/com/github/miyohide/azureblobwithenc/Main.java
+++ src/main/java/com/github/miyohide/azureblobwithenc/Main.java
@@ -1,7 +1,21 @@
 package com.github.miyohide.azureblobwithenc;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint(System.getenv("BLOB_ENDPOINT"))
+                .sasToken(System.getenv("BLOB_SAS_TOKEN"))
+                .buildClient();
+
+        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(System.getenv("CONTAINER_NAME"));
+
+        BlobClient blobClient = blobContainerClient.getBlobClient("myblockBlob");
+        blobClient.upload(BinaryData.fromString("This is Super Secret Data"));
     }
 }
