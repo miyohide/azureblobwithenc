b3b250a5f21e56c2706e01c8cdade1bc657639bd
Hidenori Miyoshi <miyohide@gmail.com>
2022-03-21 15:29:36 +0900

    refactoring

--- src/main/java/com/github/miyohide/azureblobwithenc/Main.java
+++ src/main/java/com/github/miyohide/azureblobwithenc/Main.java
@@ -8,14 +8,23 @@ import com.azure.storage.blob.BlobServiceClientBuilder;
 
 public class Main {
     public static void main(String[] args) {
+        // Blobへの接続クライアントを作成する
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                 .endpoint(System.getenv("BLOB_ENDPOINT"))
                 .sasToken(System.getenv("BLOB_SAS_TOKEN"))
                 .buildClient();
 
+        // Blobコンテナーのクライアントを作成する
         BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(System.getenv("CONTAINER_NAME"));
+        if (!blobContainerClient.exists()) {
+            blobContainerClient.create();
+        }
 
+        // Blobを作成する
         BlobClient blobClient = blobContainerClient.getBlobClient("myblockBlob");
+        if (blobClient.exists()) {
+            blobClient.delete();
+        }
         blobClient.upload(BinaryData.fromString("This is Super Secret Data"));
     }
 }
