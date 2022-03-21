a8cd6e53ebe9fa8a7060fa0f086e500a5f1b8a5e
Hidenori Miyoshi <miyohide@gmail.com>
2022-03-21 20:33:59 +0900

    set contenttype

--- src/main/java/com/github/miyohide/azureblobwithenc/Main.java
+++ src/main/java/com/github/miyohide/azureblobwithenc/Main.java
@@ -1,13 +1,22 @@
 package com.github.miyohide.azureblobwithenc;
 
-import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.specialized.BlockBlobClient;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         // Blobへの接続クライアントを作成する
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                 .endpoint(System.getenv("BLOB_ENDPOINT"))
@@ -20,11 +29,17 @@ public class Main {
             blobContainerClient.create();
         }
 
+        String blobContents = "This is a super secret data";
         // Blobを作成する
-        BlobClient blobClient = blobContainerClient.getBlobClient("myblockBlob");
+        BlockBlobClient blobClient = blobContainerClient.getBlobClient("myblob").getBlockBlobClient();
         if (blobClient.exists()) {
             blobClient.delete();
         }
-        blobClient.upload(BinaryData.fromString("This is Super Secret Data"));
+        Map<String, String> blobMetadata = Collections.singletonMap("myblobmetadata", "sample");
+        BlobHttpHeaders blobHttpHeaders = new BlobHttpHeaders().setContentDisposition("attachment")
+                .setContentType("text/plain");
+        byte[] md5 = MessageDigest.getInstance("MD5").digest(blobContents.getBytes(StandardCharsets.UTF_8));
+        InputStream dataStream = new ByteArrayInputStream(blobContents.getBytes(StandardCharsets.UTF_8));
+        blobClient.uploadWithResponse(dataStream, blobContents.length(), blobHttpHeaders, blobMetadata, null, md5, null, null, null);
     }
 }
